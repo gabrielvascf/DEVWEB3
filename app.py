cd35914dbc9a6fbe124f1a9382db7c1a5128382b
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2023-10-20 08:15:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from jinja2.exceptions import TemplateNotFound
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("home.html")
+
+@app.route("/<filename>")
+def render(filename):
+    try: 
+        return render_template(f"{filename}.html")
+    except TemplateNotFound:
+        return "Página não encontrada", 404
